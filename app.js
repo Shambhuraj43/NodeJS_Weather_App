a64a048e37818c62dc6deac53591b61e6f9916e5
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-01-02 10:05:59 -0500

    modify functions

--- app.js
+++ app.js
@@ -1,57 +1,45 @@
+//external modules
 const { copyFileSync } = require('fs');
 const { connected } = require('process');
 const request = require('request');
-const chalk = require('chalk');
 
+//local modules
 const geoCode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
+const { default: chalk } = require('chalk');
 
 
+const location = process.argv[2];
 
-// const url = 'http://api.weatherstack.com/current?access_key=5a5d0154647a471a19da88f5fabdf706&query=37.8267,-122.4233';
+if(!location){
+    console.log("Please provide a location.");
+} else{
 
-// request({ url: url, json: true}, (error, response) => {
-     
-//     if(error){
-//         console.log(chalk.red.inverse("Unable to connect to weather service!\n"));
+    //geocode function call
+    geoCode(location, (error, {latitide,longitude, placeName} = {}) => {
 
-//     } else if(response.body.error){
-//         console.log(chalk.red.inverse(response.body.error.info));
-//     }
-//     else{
-//         console.log( response.body.current.weather_descriptions[0] + ":"+ " The temperature is " + response.body.current.temperature + ". " + "It feels like " + response.body.current.feelslike);
-//     }
+    if(error){
+        return console.log(chalk.red.inverse('Error: ', error));
+    }
 
-// });
+    //forecast function call
+    forecast(latitide, longitude, (error, {description, temperature, feelsLike} = {}) => {
 
-
-
-
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2hhbWJodXJhaiIsImEiOiJja3h3OTIzdmM1d3N1MzBvNGR1aHllc250In0.eVZkrcaGra0MUMa1UwSglA&limit=1';
-
-// request({ url: geocodeURL, json: true}, (error, response) => {
-     
-//     if(error){
-//         console.log(chalk.red.inverse("Unable to connect to geocode service!\n"));
-//     }else if(response.body.features === undefined){
-//         console.log(chalk.red.inverse("Enter valid location!\n"));
-//     }
-//     else{
-//         console.log(response.body.features[0].center);
-//     }  
-// });
-
-
-// geoCode('boston', (error, data) => {
-//     console.log('Error', error);
-//     console.log('Data', data);
-// })
-
-
-forecast(-71.0596, 2.3605, (error, data) => {
-    console.log('Error: ', error);
-    console.log('Data', data);
+        if(error){
+            return console.log(chalk.red.inverse('Error: ', error));
+        }
+            console.log(chalk.black.bgBlue("Place Name:") + "    "+chalk.green.bold(placeName));
+            console.log(chalk.black.bgBlue("Description:") +"   "+ chalk.yellow.bold(description));
+            console.log(chalk.black.bgBlue("Temperature:") +"   "+ chalk.yellow.italic(temperature) + chalk.yellow.italic(" F"));
+            console.log(chalk.black.bgBlue("Feels like:") +"    "+ chalk.yellow.italic(feelsLike)+ chalk.yellow.italic(" F"));
+    });
+    
 });
+}
+
+
+
+
 
 
 // 5a5d0154647a471a19da88f5fabdf706
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,20 +1,20 @@
 const request = require('request');
 
 
-const forecast = (latitide, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=5a5d0154647a471a19da88f5fabdf706&query='+ latitide + ',' + longitude + '&units=f';
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=5a5d0154647a471a19da88f5fabdf706&query='+ latitude + ',' + longitude + '&units=f';
 
-    request({url: url , json: true}, (error, response)=> {
+    request({url: url , json: true}, (error, {body})=> {
         
         if(error){
             callback("Unable to connect to weather service!\n", undefined);
-        }else if(response.body.error){
-            callback(response.body.error.info, undefined);
+        }else if(body.error){
+            callback("Unable to find loaction!", undefined);
         }else {
             callback(undefined, {
-                description: response.body.current.weather_descriptions[0],
-                temperatur: response.body.current.temperature,
-                feelsLike: response.body.current.feelslike,
+                description: body.current.weather_descriptions[0],
+                temperature: body.current.temperature,
+                feelsLike: body.current.feelslike,
             });
             
         }
