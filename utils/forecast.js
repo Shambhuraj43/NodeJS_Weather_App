f832f86fe1c5ed13bcc844fbc26a4cefa2b048ce
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-01-02 05:16:00 -0500

    Created Utils for app

---
+++ utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+
+const forecast = (latitide, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=5a5d0154647a471a19da88f5fabdf706&query='+ latitide + ',' + longitude + '&units=f';
+
+    request({url: url , json: true}, (error, response)=> {
+        
+        if(error){
+            callback("Unable to connect to weather service!\n", undefined);
+        }else if(response.body.error){
+            callback(response.body.error.info, undefined);
+        }else {
+            callback(undefined, {
+                description: response.body.current.weather_descriptions[0],
+                temperatur: response.body.current.temperature,
+                feelsLike: response.body.current.feelslike,
+            });
+            
+        }
+    });
+}
+
+
+module.exports = forecast;
